70edef4c85dc06b92f966706231d08cdcf5eeb24
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2018-06-07 13:24:02 -0300

    menu

--- content.js
+++ content.js
@@ -5,25 +5,54 @@ let before_div_style = {
 }
 
 function getMessage(message, sender, sendResponse) {
-  console.log(message)
   if(message === 'highlight') {
     let highlighted = window.getSelection().toString();
     let times = filterTimeFromText(highlighted)
-    printResult(sumTimes(times));
+    highlitedPrint(sumTimes(times));
   }
 
   if(message === 'select') {
     let elem = document.querySelector('body');
 
-    elem.addEventListener('mouseover', addColorBackground)
-
-    elem.addEventListener('mouseout', returnNormalBackground)
-
-    elem.addEventListener('click', showResult);
+    elem.addEventListener('mouseover', addColorBackground);
+    elem.addEventListener('mouseout', returnNormalBackground);
+    elem.addEventListener('mouseover', showResult);
+    elem.addEventListener('click', removeDivResult);
   }
 }
 
+function createDivResult(){
+    let div = document.createElement('div');
+    div.style.zIndex = '99999';
+    div.id = 'popup-result';
+    div.style.width = 'auto';
+    div.style.height = 'auto';
+    div.style.padding = '10px';
+
+    div.style.border = 'solid';
+    div.style.borderColor = 'rgba(164, 66, 0, 0.6)';
+    div.style.borderWidth = '2px';
+    div.style.backgroundColor = 'rgba(255, 173, 5, 0.6)';
+    div.style.color = 'rgba(105, 20, 14, 0.9)';
+    div.style.fontSize = '40px';
+    div.style.fontFamily = 'monospace';
+
+    div.style.position = 'fixed';
+    div.style.top = '30px';
+    div.style.left = '50%';
+    return div;
+}
+
 function addColorBackground(e){
+  // if the div dont exists, create!
+  let printExist = document.querySelector('#popup-result');
+  if(!printExist) {
+    let div = createDivResult();
+
+    let body = document.querySelector('body');
+    body.appendChild(div)
+  }
+
   before_div_style.background_color = e.target.style.backgroundColor;
   e.target.style.backgroundColor = 'rgba(155, 205, 0, 0.7)';
 }
@@ -32,52 +61,40 @@ function returnNormalBackground(e) {
   e.target.style.backgroundColor = before_div_style.background_color;
 }
 
+function removeDivResult(e) {
+  let div = document.querySelector('#popup-result');
+  div.remove();
+  e.currentTarget.removeEventListener('mouseover', addColorBackground);
+  e.currentTarget.removeEventListener('mouseout', returnNormalBackground);
+  e.currentTarget.removeEventListener('mouseover', showResult);
+  e.currentTarget.removeEventListener('click', removeDivResult);
+  returnNormalBackground(e);
+}
+
 function showResult(e) {
   let text = e.target.innerText;
   let times = filterTimeFromText(text)
 
   printResult(sumTimes(times));
-
-  removeEvents(e);
-}
-
-function removeEvents(elem) {
-  elem.currentTarget.removeEventListener('mouseover', addColorBackground);
-  elem.currentTarget.removeEventListener('mouseout', returnNormalBackground);
-  elem.currentTarget.removeEventListener('click', showResult);
-  returnNormalBackground(elem);
 }
 
 function printResult(result) {
-  
-
-  let div = document.createElement('div');
-  div.className = 'popup-result';
-  div.style.width = 'auto';
-  div.style.height = 'auto';
-  div.style.padding = '10px';
-
-  div.style.border = 'solid';
-  div.style.borderColor = 'rgba(164, 66, 0, 0.6)';
-  div.style.borderWidth = '2px';
-  div.style.backgroundColor = 'rgba(255, 173, 5, 0.6)';
-  div.style.color = 'rgba(105, 20, 14, 0.9)';
-  div.style.fontSize = '40px';
-  div.style.fontFamily = 'monospace';
-
-  div.style.position = 'fixed';
-  div.style.top = '30px';
-  div.style.left = '50%';
+  let div = document.querySelector('#popup-result');
   div.textContent = result;
+}
 
-  let body = document.querySelector('body');
-  body.appendChild(div)
+function highlitedPrint(result) {
+  let printExist = document.querySelector('#popup-result');
+  if(!printExist) {
+    let div = createDivResult();
+    div.textContent = result;
+    setTimeout(()=>{
+      div.remove();
+    }, 3000);
 
-  setTimeout(function(){
-    div.remove();
-  }, 5000)
-
-  console.log(result);
+    let body = document.querySelector('body');
+    body.appendChild(div);
+  }
 }
 
 
